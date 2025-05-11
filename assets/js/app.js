d0ff7ae67ef08a32e394fb7a936d22133647ca80
Alireza Zejaji <alireza.zejaji@gmail.com>
2025-05-11 17:13:40 +0330

    Making items responsive and creating separate functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,13 @@ let search_btn = document.querySelector(".search_btn")
 let list = document.querySelector(".list")
 
 // add in list
-add_btn.addEventListener("click" , function(){
+add_btn.addEventListener("click" , add_item)
+inp_add.addEventListener("keydown" , function(e){
+    if(e.key == "Enter"){
+        add_item()
+    }
+})
+function add_item(){
     let inp_add_value = inp_add.value.toLowerCase()
     if(inp_add_value.trim() != ""){
         // show list
@@ -15,13 +21,18 @@ add_btn.addEventListener("click" , function(){
 
         // create item
         let li = document.createElement("li")
-        li.className = "alert alert-light d-flex justify-content-between mt-2 mx-1"
-        li.innerHTML =`
-        <span class="w-75">${inp_add_value}</span>
-        <div>
-            <button class="btn btn-success fw-bold btn-sm" onclick="done(this)">done</button>
-            <button class="btn btn-warning fw-bold btn-sm" onclick="doing(this)">doing</button>
-            <button class="btn-close" onclick="remove(this)"></button>
+        li.className = "alert alert-light mt-2 mx-1"
+
+        li.innerHTML = `
+        <div class="row align-items-center">
+            <div class="text col-12 col-sm-7 text-break mb-2 mb-sm-0">
+                <span class="alert_text">${inp_add_value}</span>
+            </div>
+            <div class="col-12 col-sm-5 d-flex justify-content-end gap-2 flex-wrap">
+                <i class="bi bi-check-square btn btn-outline-success fs-4 p-0 px-2" title="Done" onclick="done(this)"></i>
+                <i class="bi bi-arrow-repeat btn btn-outline-warning fs-4 p-0 px-2" title="Doing" onclick="doing(this)"></i>
+                <i class="bi bi-trash btn btn-outline-danger fs-4 p-0 px-2" title="Delete" onclick="remove(this)"></i>
+            </div>
         </div>
         `
 
@@ -31,7 +42,7 @@ add_btn.addEventListener("click" , function(){
     }
     // add item in localStorage
     localStorage.setItem("list_items" , list.innerHTML)
-})
+}
 list.innerHTML = localStorage.getItem("list_items")
 
 // hide list
@@ -40,7 +51,9 @@ if(list.innerHTML.trim() == "" ){
 }
 
 // search in list
-search_btn.addEventListener("click" , function(){
+inp_search.addEventListener("input" , search_item)
+search_btn.addEventListener("click" , search_item)
+function search_item(){
     let inp_search_value = inp_search.value
     let list_item = $(".list li")
     list_item.each(function(index , item){
@@ -50,39 +63,51 @@ search_btn.addEventListener("click" , function(){
             item.classList.remove("d-none")
         }
     })
-    inp_search.value=""
-})
+}
 
 
 // remove item
 function remove(close_btn){
     let my_alret = close_btn.closest(".alert")
-    if(confirm("remove item?")){
-        my_alret.remove()
 
-        localStorage.setItem("list_items" , list.innerHTML)
-    }
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then(function(willDelete){
+        if (willDelete) {
+            swal("Successful Remove!", "The item has removed", "success");
+            my_alret.remove()
+            localStorage.setItem("list_items" , list.innerHTML)
+        } else {
+            swal("Your item is safe!");
+        }
+    })
 
     // hide list
     if(list.innerHTML.trim() == "" ){
         list.classList.add("d-none")
     }
 }
+
 // doing item
 function doing(doing_btn){
     let my_alret = doing_btn.closest(".alert")
-    my_alret.classList.remove("alert-light" , "alert-success" , "box-shadow-green")
-    my_alret.classList.add("alert-warning" , "box-shadow-yellow")
+    my_alret.classList.remove("alert-light" , "alert-success")
+    my_alret.classList.add("alert-warning")
     let my_span = my_alret.querySelector("span")
     my_span.classList.remove("text-decoration-line-through")
 
     localStorage.setItem("list_items" , list.innerHTML)
 }
+
 // done item
 function done(done_btn){
     let my_alret = done_btn.closest(".alert")
-    my_alret.classList.remove("alert-light" , "alert-warning" ,"box-shadow-yellow")
-    my_alret.classList.add("alert-success" , "box-shadow-green")
+    my_alret.classList.remove("alert-light" , "alert-warning")
+    my_alret.classList.add("alert-success")
     let my_span = my_alret.querySelector("span")
     my_span.classList.add("text-decoration-line-through")
 
